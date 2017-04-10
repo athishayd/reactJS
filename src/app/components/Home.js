6cfc1d814d797da4adde707e481dfed37a06ede4
Athishay Jain <athishayk@gmail.com>
2017-04-10 12:31:22 +0530

    Two way binding

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -7,13 +7,24 @@ export class Home extends React.Component {
         super();
         this.state = {
             age: props.initialAge,
-            status: 0
+            status: 0,
+            homeLink: props.initialLinkName
         };
     }
 
     onMakeOlder() {
         this.setState({
             age: this.state.age + 3
+        });
+    }
+
+    onChangeName(){
+        this.props.changeLink(this.state.homeLink);
+    }
+
+    onHandleChange(event){
+        this.setState({
+            homeLink: event.target.value
         });
     }
 
@@ -26,6 +37,10 @@ export class Home extends React.Component {
                 <button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older!</button>
                 <hr/>
                 <button onClick={this.props.greet} className="btn btn-primary">Greet</button>
+                <hr/>
+                <input type="text" value={this.state.homeLink} 
+                                    onChange={(event) => this.onHandleChange(event)} />
+                <button onClick={this.onChangeName.bind(this)} className="btn btn-primary">Change header link</button>
             </div>
         );
     }
@@ -34,5 +49,6 @@ export class Home extends React.Component {
 Home.propTypes = {
     name: PropTypes.string,
     initialAge: PropTypes.number,
-    greet: PropTypes.func
+    greet: PropTypes.func,
+    initialLinkName: PropTypes.string
 }
